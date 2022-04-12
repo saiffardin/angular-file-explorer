29295826bbc3a4c6012169cdefd206bacdc6597a
saiffardin <saiffardin@gmail.com>
2022-04-12 11:25:22 +0600

    file explorer module - impotred

--- src/app/file-explorer/file-explorer.module.ts
+++ src/app/file-explorer/file-explorer.module.ts
@@ -2,11 +2,38 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatIconModule } from '@angular/material/icon'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatMenuModule } from '@angular/material/menu'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+import { FormsModule } from '@angular/forms'
+import { FileExplorerComponent } from './file-explorer.component'
+
+
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatGridListModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+  ],
+  entryComponents: [],
+  declarations: [FileExplorerComponent],
+  exports: [FileExplorerComponent],
+
+
 })
 export class FileExplorerModule { }
